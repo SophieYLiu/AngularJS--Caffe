9f0dcdd094864664ec776c910bf0d55ee61b59bf
tjgrst <tjgrst@gmail.com>
2016-06-16 14:31:41 -0500

    added json file for coffees

--- Caffe/public/javascripts/coffee-list/coffee-list.component.js
+++ Caffe/public/javascripts/coffee-list/coffee-list.component.js
@@ -6,33 +6,12 @@ angular.
 module("caffeApp").
 component("coffeeList", {
     templateUrl: "javascripts/coffee-list/coffee-list.template.html",
-    controller: function CoffeeListController() {
-        this.coffees = [
-            {
-                name: "Java Time",
-                description: "Java Time is the creme de la creme of Indonesian coffees.",
-                roast: "Medium"
-            }, {
-                name: "Express Espresso",
-                description: "Express will give you he jolt you need at 2pm.",
-                roast: "Dark"
-            },{
-                name: "Morning Jo",
-                description: "One of our most popular morning brews.",
-                roast: "Light"
-            }, {
-                name: "Exquisite Roast",
-                description: "Java Time is the creme de la creme of Indonesian coffees.",
-                roast: "Medium"
-            }, {
-                name: "Guatemalan",
-                description: "Express will give you he jolt you need at 2pm.",
-                roast: "Light"
-            },{
-                name: "Madagascar",
-                description: "One of our most popular morning brews.",
-                roast: "Dark"
-            }
-        ];
+    controller: function CoffeeListController($http) {
+        var self = this;
+        self.orderProp = "name";
+
+        $http.get("coffees/coffees.json").then(function(response){
+            self.coffees = response.data;
+        });
     }
 });
