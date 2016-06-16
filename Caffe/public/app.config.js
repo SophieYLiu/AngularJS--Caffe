be12f8b1bdd08b9ea20c313080007f4454669818
tjgrst <tjgrst@gmail.com>
2016-06-16 17:08:31 -0500

    spaced out shop layout

---
+++ Caffe/public/app.config.js
@@ -0,0 +1,21 @@
+// /**
+//  * Created by tjgrst on 6/16/2016.
+//  */
+// 'use strict"
+//
+// angular.
+// module('caffeApp').
+// config(['$locationProvider', '$routeProvider',
+//     function config($locationProvider, $routeProvider) {
+//         $locationProvider.hashPrefix('!');
+//
+//         $routeProvider.
+//         when('/shop', {
+//             template: '<phone-list></phone-list>'
+//         }).
+//         when('/shop/:name', {
+//             template: '<coffee-detail></coffee-detail>'
+//         }).
+//         otherwise('/shop');
+//     }
+// ]);
